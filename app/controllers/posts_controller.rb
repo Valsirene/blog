edb454ae3ea5f9186edd567e6a50696ff3809c52
Valsirene <valsirenesp@gmail.com>
2019-09-09 20:26:49 -0300

    #1:Mnutenção de posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,22 @@ class PostsController < ApplicationController
     def new
         @post = Post.new
     end
+    def create
+        @post = Post.new(post_params)
+        @post.save
+        redirect_to @post
+    end
+    
+    def show
+        @post = Post.find(params[:id])
+    end
+    def new
+        @post = Post.new
+    end
+
+    private 
+    def post_params
+       params.require(:post).permit(:title, :body)
+    end
+
 end
