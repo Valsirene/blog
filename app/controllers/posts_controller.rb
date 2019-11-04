a2791cd18bb1858697d13dfa19f9d245d375f047
Valsirene <valsirenesp@gmail.com>
2019-11-04 21:22:57 -0200

    #1:manutencao de posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,7 @@ class PostsController < ApplicationController
     def new
         @post = Post.new
     end
-
-    def create
-        @post = Post.new(post_params)
-        @post.save
-        redirect_to @post
-    end
-    
+       
     def show
         @post = Post.find(params[:id])
     end
@@ -25,21 +19,39 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end
 
-    def update
-        @post = Post.find(params[:id])
-        @post.update(post_params)
-        redirect_to @post
-    end
-
-    def destroy
-        @post = Post.find(params[:id])
-        @post.destroy
-        redirect_to posts_path
-    end
-
+       
     private 
     def post_params
        params.require(:post).permit(:title, :body)
     end
+    def create
+        @post = Post.new(post_params)
+     if @post.save
+       redirect_to posts_path,
+            notice:'Post  was  sucessfully  created!'
+     else
+        render:new
+      end
+    end
 
-end
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to  posts_path,
+                notice:'Post  was  sucessfully  update!'
+            else
+                render:edit
+            end
+        end
+    
+        def destroy
+        @post = Post.find(params[:id])
+        if  @post.destroy
+            redirect_to  posts_path,
+                notice:'Post  was  sucessfully  removed!' 
+        else
+            redirect_to  posts_path,
+            alert:'Fail to  remove  post!'
+        end
+      end
+    end
